57658feeada0261f8a21a9b219ba68c8c3984f8a
Atit Kharel <atit286@gmail.com>
2022-12-25 21:38:05 +0545

    updated env variables

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 import tweepy
-from secrets import *
-
+from dotenv import load_dotenv
+load_dotenv()
+import os
 import logging
-
 
 logger = logging.getLogger()
 
 def create_api():
-    consumer_key = key[0]
-    consumer_secret = key[1]
-    access_token = key[2]
-    access_token_secret = key[3]
+    consumer_key = os.environ.get("CONSUMER_KEY")
+    consumer_secret = os.environ.get("CONSUMER_SECRET")
+    access_token = os.environ.get("ACCESS_TOKEN")
+    access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+    api = tweepy.API(auth)
     try:
         api.verify_credentials()
     except Exception as e:
--- secrets.py
+++
@@ -1,5 +0,0 @@
-key = ['jo2Amcr0ceh5BtT5Bkh1wu4H5',
-      'r6VYrLeqT3yfebfGEfp5vX7YrMoUO9j4Cx9QqAZSeLQRlX1Agi',
-      '1314087797729325056-HzNoUpy6rV8QbGUqmFc1LBJ3vsAixD',
-      '1gju1JFJtdmcsJvYZFUAI9XjKurx7PODFmzlprXjb2Hzf'
-]
